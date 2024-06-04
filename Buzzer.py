f195d6ea58438e9c562f69b2e3072cb68342cbf5
Ardhasaputra <ardhasaputra10@gmail.com>
2024-06-05 06:56:13 +0700

    update Buzzer.py

--- Buzzer.py
+++ Buzzer.py
@@ -1,5 +1,13 @@
-def alarm ():
-    print("alarm is start")
-
-print()
-print()
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+buzzer=18
+GPIO.setup(buzzer,GPIO.OUT)
+while True:
+    GPIO.output(buzzer, GPIO.HIGH)
+    print("Beep")
+    sleep(0.5)
+    GPIO.output(buzzer, GPIO.LOW)
+    print("No Beep")
+    sleep(0.5)
